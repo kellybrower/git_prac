8aeee3b30c483bbf03b585add62a7c0d72086f32
Kelly Brower <kbrower77@gmail.com>
2021-02-23 15:01:57 -0800

    adding fun.py

---
+++ fun.py
@@ -0,0 +1,19 @@
+# git_handson/play.py
+from git_prac import Goober_Bear
+
+def make_play(goober):
+    """
+    Here we just call a Goober Bear object and ask it to play.
+    """
+    goober.play()
+
+
+if __name__ == "__main__":
+    Goober = Goober_Bear()
+    make_play(Goober)
+
+#adding
+#well
+#multiple lines
+def nada():
+    pass
