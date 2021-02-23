744910ab003a92bf1e1d11160ad0fe0837c5caeb
Kelly Brower <kbrower77@gmail.com>
2021-02-23 12:05:06 -0800

    first commit

---
+++ git_prac.py
@@ -0,0 +1,18 @@
+#git_handon/git_prac.py
+
+class Goober_Bear:
+    def ooze(self):
+        self.ooze = "I'm oozing, please leave me be."
+        ooze = self.ooze
+        print(ooze)
+
+    def play(self):
+        self.play = "Let's play!"
+        play = self.play
+        print(play)
+
+
+if __name__ == "__main__":
+    Grendel = Goober_Bear()
+    Grendel.ooze()
+    Grendel.play()
